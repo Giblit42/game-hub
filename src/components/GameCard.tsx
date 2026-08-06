cb85720829190c94cba79c9e523769a6fae52399
Giblit42 <71239011+Giblit42@users.noreply.github.com>
2026-08-06 07:08:15 -0500

    Changed Casing

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,7 +2,7 @@ import { Game } from "../hooks/useGames";
 import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react";
 import { PlatformIconList } from "./PlatformIconList";
 import CriticScore from "./CriticScore";
-import getCroppedImageUrl from "../Services/Image-Url";
+import getCroppedImageUrl from "../Services/image-url";
 
 interface Props {
   game: Game;
